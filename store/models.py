ca1f9ea59b4ea8cb8f3453035d469f4160cbec24
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-02-02 17:16:49 +0100

    created models Category, Product newly created app added into the installed apps

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    parent_category = models.ForeignKey('self')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(max_length=10)
+    categories = models.ManyToManyField(Category)
+
