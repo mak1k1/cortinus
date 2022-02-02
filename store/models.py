6357a6be1ba5d728925afa9fb77975f077e95041
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-02-02 17:23:18 +0100

    migrated

--- store/models.py
+++ store/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    parent_category = models.ForeignKey('self')
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity = models.IntegerField(max_length=10)
+    quantity = models.IntegerField()
     categories = models.ManyToManyField(Category)
 
