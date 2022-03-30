c2c0aaad116b5c16f1f19a2c8b3d474cf6fdaf47
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-03-30 14:14:56 +0200

    Added __str__ to all models

--- store/models.py
+++ store/models.py
@@ -19,6 +19,9 @@ class Author(models.Model):
     surname = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
 
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
 
 class Book(models.Model):
     name = models.CharField(max_length=50)
