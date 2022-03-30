4d5260fa6993e14f4b3408ed6620d9dfa1073bcc
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-03-30 21:08:46 +0200

    Introduced Rest API via Django Rest Framework, still some cleanup needed in templates, views.py  and urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,15 @@
-from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView
+from rest_framework import routers
+from store import views
+
+router = routers.DefaultRouter()
+router.register(r'authors', views.AuthorViewSet)
+router.register(r'books', views.BookViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'products', views.ProductViewSet)
+
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-    path('<slug:category_slug>', HomeView.as_view())
+    path('', include(router.urls)),
+    path('api/', include('rest_framework.urls', namespace='cortinus_rest_framework'))
 ]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,52 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Product, Category
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import viewsets
+from store.models import Author, Book, Category, Product
+from store.serializers import AuthorSerializer, BookSerializer, CategorySerializer, ProductSerializer
 
 
-class HomeView(ListView):
-    model = Product
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Authors to be viewed or edited
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
 
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Authors to be viewed or edited
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Authors to be viewed or edited
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Authors to be viewed or edited
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# from django.shortcuts import render
+# from django.views.generic import ListView, DetailView
+# from .models import Product, Category
+#
+#
+# class HomeView(ListView):
+#     model = Product
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['categories'] = Category.objects.all()
+#         return context
