8e97005a5d9f470706ec8cccdfbae770fec55906
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-19 15:39:50 +0200

    Added models Category and Book

--- books/models.py
+++ books/models.py
@@ -1,11 +1,30 @@
+from re import M
 from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
         return self.name
-    
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField("Author")
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    description = models.TextField()
+    categories = models.ManyToManyField("Category")
+
+    def __str__(self):
+        return self.title
+    
